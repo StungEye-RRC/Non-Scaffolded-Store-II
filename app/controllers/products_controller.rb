5e1d5e0a4c7d437ef2c18098d36d5c2f18623aa6
Kyle Geske <stungeye@gmail.com>
2015-03-18 13:50:50 -0500

    Implemented the rest of the RESTful actions for Products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,52 @@ class ProductsController < ApplicationController
   # By default, after a controller action is execute an associated view
   # will be auto-loaded: app/views/products/index.html.erb
   # This view will have access to all the action's instance variables.
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+  # By default, the app/views/products/show.html.erb will be loaded with
+  # access to the show method's instance variables.
+  
+  def new
+    @product = Product.new
+  end
+  # app/views/products/new.html.erb
+  
+  def create
+    @product = Product.new(product_params)
+    
+    if (@product.save)
+      redirect_to @product
+    else
+      render :new
+    end
+    
+  end
+  # No associated view, so there is no need for app/views/products/create.html.erb
+  
+  def edit
+    @product = Product.find(params[:id])
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    if (@product.update_attributes(product_params))
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+  # No associated view here. Just a redirect or a render.
+  
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to root_path
+  end
+  
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,34 @@
 Rails.application.routes.draw do
+  
+  # READING
+  
   # When I receive a GET request to /products
   # load the index action of the products controller.
   get 'products' => 'products#index'
   # ^Verb  ^URL       ^Controller ^action
+  
+  # When we receive a GET request to /products/#
+  # load the show action of the products controller.
+  # The value of the url at the position :id ends up
+  # in the params hash at params[:id]
+  get 'products/:id' => 'products#show', as: 'product',  id: /\d+/ 
+  
+  # CREATING
+  
+  # A Route to display a new product form.
+  get 'products/new' => 'products#new', as: 'new_product'
+  post 'products' => 'products#create', as: 'create_product'
+  
+  # UPDATING
+  
+  get 'products/:id/edit' => 'products#edit', as: 'edit_product',  id: /\d+/ 
+  patch 'products/:id' => 'products#update',   id: /\d+/ 
+  
+  # DELETING
+  
+  delete 'products/:id' => 'products#destroy', as: 'delete_product', id: /\d+/ 
+  
+  # A Route to handle the data posted by our new product form.
   
   root to: 'products#index'
 end
